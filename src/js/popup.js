566839b3666b94dea7bbc67ce213d5555a640afa
Masayuki Goto <originals0711@gmail.com>
2018-08-30 14:53:17 +0900

    add fontawesome setting

--- src/js/popup.js
+++ src/js/popup.js
@@ -2,5 +2,6 @@ import "../css/popup.css";
 import hello from "./popup/example";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 //hello();
